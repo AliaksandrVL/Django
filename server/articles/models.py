ddf058a2737538e29e32d15206375d29fe0103cb
AliaksandrVL <litvinuk2@gmail.com>
2018-07-24 11:08:57 +0300

    New field 'category' was added as foreign key

--- server/articles/models.py
+++ server/articles/models.py
@@ -4,6 +4,7 @@ class Article(models.Model):
     name = models.CharField(max_length=150)
     image = models.ImageField()
     content = models.TextField()
+    category = models.ForeignKey('categories.Category', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
