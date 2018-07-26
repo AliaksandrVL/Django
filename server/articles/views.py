29576880303b422ac6bca9d9ca62bf18c756c82a
AliaksandrVL <litvinuk2@gmail.com>
2018-07-26 08:42:17 +0300

    New view, get_list_or_404 was added

--- server/articles/views.py
+++ server/articles/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
 from django.http import HttpResponse
 
 from . import models
 
-def index(request):
-
-    query = models.Article.objects.all()
+def article_list(request):
+    query = get_list_or_404(models.Article)
 
     return render(request, 'articles/index.htm', {'query': query})
+
+def article_detail(request, pk):
+    instance = get_list_or_404(models.Article, id=pk)
+
+    return render(request, 'articles/detail.htm', {'instance': instance})
