69b23e5fa7419a373cb1ab0892d96a7e297cd5fc
AliaksandrVL <litvinuk2@gmail.com>
2018-07-31 07:28:58 +0300

    New url for 'security'

--- server/server/urls.py
+++ server/server/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
+    re_path(r'^auth/', include('security.urls')),
     re_path(r'^', include('articles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
